07798239318aee1206a65312b94efddc87ecabc0
Joshua Kennedy <joshkennede@Joshuas-MacBook-Pro.local>
2016-08-22 22:10:01 -0700

    Initial commit

---
+++ src/com/studentroster/Roster.java
@@ -0,0 +1,99 @@
+package com.studentroster;
+import java.util.ArrayList;
+/**
+ * Created by joshkennede on 8/22/16.
+ */
+
+public class Roster {
+
+    private static ArrayList<Student> students = new ArrayList<>();
+
+    //main method to run the program
+    public static void main(String[] args)
+    {
+        String [] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
+                            "2,Suzan,Erickson,Erickson_1990@gmailcom,19,91,72,85",
+                            "3,Jack,Napoli,The_lawyer99yahoo.com,19,85,84,87",
+                            "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
+                            "5,Josh,Kennedy,jkenn66@wgu.edu,29,80,94,70"};
+
+        // Split the input string by comma
+        for(int i = 0; i < students.length; i++)
+
+        {
+            String characters = students[i];
+            String[] arrayVar = characters.split(",");
+            String studentId = arrayVar[0];
+            String firstName = arrayVar[1];
+            String lastName = arrayVar[2];
+            String email = arrayVar[3];
+
+            int age = Integer.parseInt(arrayVar[4]);
+            int grade1 = Integer.parseInt(arrayVar[5]);
+            int grade2 = Integer.parseInt(arrayVar[6]);
+            int grade3 = Integer.parseInt(arrayVar[7]);
+
+            // use the add method to add a student to the students ArrayList
+            Roster.add(studentId, firstName, lastName, email, age, grade1, grade2, grade3);
+        }
+
+        Roster.print_all();
+        Roster.print_invalid_emails();
+        Roster.print_average_grade("1");
+        Roster.remove("3");
+        Roster.remove("3");
+    }
+
+    public static void add(String studentId, String firstName, String lastName, String email, int age, int grade1, int grade2, int grade3)
+    {
+        Student student = new Student(studentId, firstName, lastName, email, age, grade1, grade2, grade3);
+        students.add(student);
+    }
+    //method for removing a student from the roster
+    public static void remove(String studentId)
+    {
+        for (int i = 0; i < students.size(); i++)
+        {
+            if (students.get(i).getStudentId().equals(studentId))
+            {
+                students.remove(i);
+                System.out.println("Student " + studentId + " has been removed.");
+                return;
+            }
+        }
+        System.out.println("Error: Student ID " + studentId + " can not be found.");
+    }
+    //method for printing the entire student arrayList
+    public static void print_all()
+    {
+        for (Student student : students)
+        {
+            student.print();
+        }
+    }
+    //method for printing invalid emails
+    public static void print_invalid_emails()
+    {
+        String invalidEmail = "Invalid email address: ";
+        for(Student Student : students)
+        {
+            String emailsInRoster = Student.getEmail();
+            if (!(emailsInRoster.contains("@")) || (emailsInRoster.contains(" ")) || !(emailsInRoster.contains(".")))
+            {
+                System.out.println(invalidEmail + emailsInRoster);
+            }
+        }
+    }
+    //method for printing the average grade of a given student
+    public static void print_average_grade(String studentId)
+    {
+        for (Student student : students) {
+            if (student.getStudentId().equals(studentId)) {
+                double totalGrades = student.getGrade1() + student.getGrade2() + student.getGrade3();
+                double gradeAverage = totalGrades / 3;
+
+                System.out.println("Student " + studentId + "'s average grade overall is " + gradeAverage);
+            }
+        }
+    }
+}
---
+++ src/com/studentroster/Student.java
@@ -0,0 +1,130 @@
+package com.studentroster;
+
+/**
+ * Created by joshkennede on 8/22/16.
+ */
+public class Student {
+
+    //Student class which includes: student ID, first name, last name, e-mail address, age, and array of grades.
+    private String studentId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private int grade1;
+    private int grade2;
+    private int grade3;
+    private final int GRADES = 3;
+    int[] grades = new int[GRADES];
+
+    //Student constructor
+    public Student(String studentId, String firstName, String lastName,
+                   String email, int age, int grade1, int grade2, int grade3)
+    {
+        this.studentId = studentId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        setGrade1(grade1);
+        setGrade2(grade2);
+        setGrade3(grade3);
+    }
+    //accessor for studentId
+    public String getStudentId()
+    {
+        return studentId;
+    }
+    //mutator for changing student id
+    private void setStudentId(String studentId)
+    {
+        this.studentId = studentId;
+    }
+    //accessor for firstName method
+    public String getFirstName()
+    {
+        return firstName;
+    }
+    //mutator for firstName method
+    private void setFirstName(String firstName)
+    {
+        this.firstName = firstName;
+    }
+    //accessor for lastName method
+    public String getLastName()
+    {
+        return lastName;
+    }
+    //mutator for lastName method
+    private void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+    //accessor for getEmail method
+    public String getEmail()
+    {
+        return email;
+    }
+    //mutator for changing email
+    private void setEmail(String eMail)
+    {
+        this.email = eMail;
+    }
+    //accessor for reading student age
+    public int getAge()
+    {
+        return age;
+    }
+    //mutator for setAge method - changing students age
+    private void setAge(int age)
+    {
+        this.age = age;
+    }
+    //accessor for reading first grade in array
+    public int getGrade1()
+    {
+        return grades[0];
+    }
+    //mutator for changing first grade in array
+    private void setGrade1(int grade1)
+    {
+        this.grades[0] = grade1;
+    }
+    //accessor for reading second grade in array
+    public int getGrade2()
+    {
+        return grades[1];
+    }
+    //mutator for changing second grade in array
+    private void setGrade2(int grade2)
+    {
+        this.grades[1] = grade2;
+    }
+    //accessor for reading third grade in array
+    public int getGrade3()
+    {
+        return grades[2];
+    }
+    //mutator for changing third grade in array
+    private void setGrade3(int grade3)
+    {
+        this.grades[2] = grade3;
+    }
+    //method for printing all student data
+    public void print()
+    {
+        String tab = "   ";
+        String studentId = getStudentId();
+        String firstName = getFirstName();
+        String lastName = getLastName();
+        String email = getEmail();
+        int age = getAge();
+        int grade1 = getGrade1();
+        int grade2 = getGrade2();
+        int grade3 = getGrade3();
+
+        System.out.println("Student ID: " +studentId+tab+ " Firstname: " +firstName+tab+ " Lastname: "
+                +lastName+tab+ " Email: " +email+tab+ " Age: " +age+tab+ " Grades: " +tab+ "{"+grade1+ ","
+                +grade2+ "," +grade3+ "}");
+    }
+}
